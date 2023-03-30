c55eca3673503c2f0b47a494934482c5aec628f5
Vyasheslav Derevyanko <malorka0540205205@mail.ru>
2023-03-30 22:29:35 +0300

    done

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,9 +1,38 @@
 package practice;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
-        // TODO: написать консольное приложение для работы со списком дел todoList
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String add = scanner.nextLine();
+            String[] first = add.split("\s");
+            String firstWord = first[0];
+            String lastWord = add.replaceFirst(firstWord + "\s", "");
+
+            switch (firstWord) {
+                case ("ADD") -> {
+                    todoList.add(lastWord);
+                    System.out.println("Добавлено дело" + todoList.getTodos());
+                }
+                case ("DELETE") -> {
+                    todoList.delete(Integer.parseInt(lastWord));
+                    System.out.println("Дело удалено" + todoList.getTodos());
+                }
+                case ("EDIT") ->{
+                    String finalWord = first[1];
+                    lastWord = lastWord.replaceFirst(finalWord,"");
+                    todoList.edit(Integer.parseInt(finalWord),lastWord);
+                    System.out.println("Дело " + firstWord + " Заменено на" + lastWord);}
+                case ("LIST") ->{
+                    System.out.println("Список дел: " +todoList.getTodos());
+                }
+            }
+        }
     }
 }
--- src/main/java/practice/TodoList.java
+++ src/main/java/practice/TodoList.java
@@ -3,29 +3,26 @@ package practice;
 import java.util.ArrayList;
 
 public class TodoList {
-
+    ArrayList<String> todolist = new ArrayList<>();
     public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
-    }
+       todolist.add(todo);}
 
     public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+       if(index<todolist.size() && index >= 0){
+           todolist.add(index,todo);}
+       else todolist.add(todo);
     }
 
     public void edit(int index, String todo) {
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
-    }
+        if(index<todolist.size())
+        todolist.set(index,todo);}
 
     public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
-    }
+        if(index<todolist.size())
+        todolist.remove(index);}
 
     public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
-    }
+
+        return todolist;}
 
 }
